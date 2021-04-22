d07b31580580004aad6f5d4ca8e262d5a02554c1
YBK <ybkamaleri@users.noreply.github.com>
2021-04-22 10:45:55 +0200

    feat: Add skip function

--- udir/udir-start.R
+++ udir/udir-start.R
@@ -1,19 +1,20 @@
 
 ## UDir fil
-udirfil <- "f:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/VALIDERING/kodechk/udir/20210322-1509_GSK.ElevundersoekelsenG.csv"
+udirfil <- "f:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/VALIDERING/kodechk/udir/20210319-1109_GSK.ElevundersoekelsenG.csv"
 
 ## KUBE fil
-kubefil <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2021NESSTAR/MOBBING_1aar_0_2020-08-14-13-13.csv"
+kubefil <- "f:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR/MOBBING_1aar_0_2021-04-21-10-01.csv"
 
 ## Which columns in the Udir raw data
 Kommunekode = "D"
 Organisasjonsnummer = "E"
 Kolonne = c("J", "K", "L")
+uskip = TRUE #FALSE hvis første linje udir csv fil ikke starter med skip=
 
 ## What these columns have
-J = c(aar = 2019, kjonn = 0, trinn = 7)
-K = c(aar = 2019, kjonn = 1, trinn = 7) 
-L = c(aar = 2019, kjonn = 2, trinn = 7)
+J = c(aar = 2016, kjonn = 0, trinn = 7)
+K = c(aar = 2016, kjonn = 1, trinn = 7) 
+L = c(aar = 2016, kjonn = 2, trinn = 7)
 
 
 
@@ -21,3 +22,5 @@ L = c(aar = 2019, kjonn = 2, trinn = 7)
 if (!require(devtools)) install.packages("devtools")
 devtools::source_url("https://github.com/helseprofil/misc/blob/main/udir/udir-check.R?raw=TRUE")
 udir_check()
+
+
