9478e7e253768d3c04e12b6912b86d7dee0a5894
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-03-13 16:00:15 +0100

    Clone user branch for reproducibility

--- utils.R
+++ utils.R
@@ -18,5 +18,37 @@ kh_install <- function(pkg = c("orgdata", "norgeo", "KHompare", "bat2bat")){
 }
 
 
+## Clone user branch for reproducibility ie. keep the same package version for
+## dependencies
+kh_clone <- function(pkg = c("orgdata", "norgeo", "KHompare", "bat2bat")){
+
+  pkg <- match.arg(pkg)
+  if (length(pkg) > 1) stop("Can't install more than one package at a time!")
+
+  pkgs <- c("gert", "fs", "renv")
+  sapply(pkgs, function(x) {
+    if(!requireNamespace(x))
+      install.packages(x, repos = "https://cloud.r-project.org/")})
+
+  khPath <- file.path(fs::path_home(), pkg)
+
+  if (!fs::dir_exists(khPath)){
+    message("Create folder ", khPath)
+    fs::dir_create(khPath)
+    setwd(khPath)
+
+    khRepo <- paste0("helseprofil/", pkg)
+    gert::git_clone(khRepo, branch = "user")
+    renv::restore()
+  } else {
+    setwd(khPath)
+    gert::git_pull()
+    renv::restore()
+  }
+
+  invisible()
+}
+
+
 ## To use, run this code:
 ## source("https://raw.githubusercontent.com/helseprofil/misc/main/utils.R")
