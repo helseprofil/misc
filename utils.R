64aa2fb77fa6c47ac82f245c0527867d5897bc98
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-03-13 16:26:09 +0100

    Clean breakpoints

--- utils.R
+++ utils.R
@@ -21,8 +21,6 @@ kh_install <- function(pkg = c("orgdata", "norgeo", "KHompare", "bat2bat")){
 ## Clone user branch for reproducibility ie. keep the same package version for
 ## dependencies
 kh_clone <- function(pkg = c("orgdata", "norgeo", "KHompare", "bat2bat")){
-  ##:ess-bp-start::browser@nil:##
-  browser(expr=is.null(.ESSBP.[["@2@"]]));##:ess-bp-end:##
 
   pkg <- match.arg(pkg)
   if (length(pkg) > 1) stop("Can't install more than one package at a time!")
