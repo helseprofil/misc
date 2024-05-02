90ef02b4ac9e8b4f475c31122b3218f38daeebd1
Vegard Lysne <vegard.lysne@fhi.no>
2024-05-02 14:00:38 +0200

    Add ProfileSystems() function

---
+++ ProfileSystems.R
@@ -0,0 +1,108 @@
+#' getProfileSystem
+#' 
+#' This function installs all packages and projects needed for FHP/OVP-related work
+#' 
+#' If path is not provided, C:/Users/name/helseprofil will be the root folder for other
+#' projects to be installed into. 
+#' 
+#' @param path root folder where you want the projects to be stored
+#' 
+#' @examples
+#' 
+#' getProfileSystem()
+#' getProfileSystem(path = "Your/Preferred/Path)
+#' 
+getProfileSystem <- function(path = NULL){
+  
+packages <- c("collapse",
+              "conflicted",
+              "data.table",
+              "DBI",
+              "dplyr",
+              "DT",
+              "epitools",
+              "forcats",
+              "foreign",
+              "fs",
+              "ggforce",
+              "ggh4x",
+              "ggplot2",
+              "ggtext",
+              "httr2",
+              "intervals",
+              "pak",
+              "plyr",
+              "purrr",
+              "readxl",
+              "RODBC",
+              "sas7bdat",
+              "sqldf",
+              "stringr",
+              "XML",
+              "zoo")
+
+message("\nInstalling CRAN packages...")
+options(warn = -1)
+suppressPackageStartupMessages(sapply(packages, 
+                                      require, 
+                                      character.only = TRUE))
+options(warn = TRUE)
+
+# Install packages from GitHub
+message("\nInstalling norgeo...")
+pak::pkg_install("helseprofil/norgeo")
+message("\nInstalling orgdata...")
+pak::pkg_install("helseprofil/orgdata")
+
+# Set base folder for installing projects. Always create the helseprofil folder as well. 
+message("\nGenerating folders:")
+helseprofil <- file.path(fs::path_home(), "helseprofil")
+if(!fs::dir_exists(helseprofil)){
+  fs::dir_create(helseprofil)
+  cat("\n- ", helseprofil)
+} else {
+  cat("\n- ", helseprofil, " already exists")
+}
+
+if(is.null(path)){
+  path <- helseprofil
+} 
+  
+if(!fs::dir_exists(path)){
+    fs::dir_create(path)
+    cat("\n- ", path)
+}
+
+message("\n\nR Projects will be installed into ", path)
+
+message("\nInstalling khfunctions (master branch)...")
+
+khfunctions_repo <- "https://github.com/helseprofil/khfunctions.git"
+khfunctions_dir <- file.path(path, "khfunctions")
+if(fs::dir_exists(khfunctions_dir)){
+  setwd(khfunctions_dir)
+  invisible(system("git fetch origin master"))
+  invisible(system("git reset --hard origin/master"))
+  invisible(system("git pull"))
+  message("khfunctions master branch restored to current GitHub version")
+} else {
+  invisible(system(paste("git clone", khfunctions_repo, khfunctions_dir)))
+  message("khfunctions cloned into: ", khfunctions_dir)
+}
+
+message("\nInstalling KHvalitetskontroll (main branch)...")
+
+KHvalitetskontroll_repo <- "https://github.com/helseprofil/KHvalitetskontroll.git"
+KHvalitetskontroll_dir <- file.path(path, "KHvalitetskontroll")
+if(fs::dir_exists(KHvalitetskontroll_dir)){
+  setwd(KHvalitetskontroll_dir)
+  invisible(system("git fetch origin main"))
+  invisible(system("git reset --hard origin/main"))
+  invisible(system("git pull"))
+  message("KHvalitetskontroll main branch restored to current GitHub version")
+} else {
+  invisible(system(paste("git clone", KHvalitetskontroll_repo, KHvalitetskontroll_dir)))
+  message("KHvalitetskontroll cloned into: ", KHvalitetskontroll_dir)
+}
+
+}
